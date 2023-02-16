dc26e70abd2e2214c8cb320207e5877745cf05b6
Kiet Truong <kietb2016977@student.ctu.edu.vn>
2023-02-16 10:32:53 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const app = express();
 
 const contactsRouter = require("./app/routes/contact.route");
 
+const ApiError = require("./app/api-error");
+
+
+app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+    });
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
+    
+   
 app.use(cors());
 app.use(express.json());
 
