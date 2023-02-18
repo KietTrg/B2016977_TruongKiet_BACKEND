0fb53745cdd6ca099a0be7db870896b510ca7926
Kiet Truong <kietb2016977@student.ctu.edu.vn>
2023-02-18 15:45:38 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -9,16 +9,8 @@ const ApiError = require("./app/api-error");
 
 
 app.use("/api/contacts", contactsRouter);
-app.use((req, res, next) => {
-    return next(new ApiError(404, "Resource not found"));
-    });
-app.use((err, req, res, next) => {
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
-    });
-});
-    
-   
+
+
 app.use(cors());
 app.use(express.json());
 
@@ -28,4 +20,12 @@ app.get("/", (req, res) => {
 
 app.use("/api/contacts", contactsRouter);
 
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+    });
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const app = require("./app");
-const config = require("./app/config");
+const app = require('./app');
+const config = require('./app/config');
+const MongoDB = require("./app/utils/mongodb.util");
 
-const POST = config.app.port;
-app.listen(POST,() => {
-    console.log(`Server is running on port ${POST}.`);
-});
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log('Connected to the database');
+
+        //start server
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}.`);
+        });
+    }
+    catch (error) {
+        console.log('Cannot connect to database', error);
+        process.exit();
+    }
+}
+
+startServer();
